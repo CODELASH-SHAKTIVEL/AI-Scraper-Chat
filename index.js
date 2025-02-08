92eac80bf275082e2ad8d79ea93425731e54fbe8
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2025-02-08 22:55:00 +0530

    Feat : Docker-compose &  ChromaDb setup completed

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ import * as cheerio from "cheerio";
 import OpenAI from "openai";
 dotenv.config();
 
+import { ChromaClient } from "chromadb";
+const chromaClient = new ChromaClient({
+  path: "<Your Chroma instance IP>",
+  port: 8000,
+});
+chromaClient.heartbeat();
+const WEB_COLLECTION = `WEB_SCAPED_DATA_COLLECTION-1`;
+
 const openai = new OpenAI(process.env.OpenAI_API_Key);
 
 const scrapewebappinfo = async (url = "") => {
@@ -14,8 +22,8 @@ const scrapewebappinfo = async (url = "") => {
   const pageHead = $("head").html();
   const pageBody = $("body").html();
 
-  const internalLinks = [];
-  const externalLinks = [];
+  const internalLinks = new Set();
+  const externalLinks = new Set();
 
   $("a").each((_, element) => {
     const link = $(element).attr("href");
@@ -24,13 +32,18 @@ const scrapewebappinfo = async (url = "") => {
       return;
     }
     if (link.startsWith("http") || link.startsWith("https")) {
-      externalLinks.push(link);
+      externalLinks.add(link);
     } else {
-      internalLinks.push(link);
+      internalLinks.add(link);
     }
   });
 
-  return { head: pageHead, body: pageBody, internalLinks, externalLinks };
+  return {
+    head: pageHead,
+    body: pageBody,
+    internalLinks: Array.from(internalLinks),
+    externalLinks: Array.from(externalLinks),
+  };
 };
 
 const generatevectorEmbeddings = async ({ text }) => {
@@ -41,6 +54,44 @@ const generatevectorEmbeddings = async ({ text }) => {
   });
   return embedding.data[0].embedding;
 };
+
+async function ingest(url = "") {
+  try {
+    console.log(`Ingesting ${url}`);
+    const { head, body, internalLinks } = await scrapeWebsite(url);
+    const headEmbedding = await generatevectorEmbeddings({ text: head });
+    insertIntoDb({ embedding: headEmbedding, url });
+    const bodyChunks = chunkText(body, 1000);
+    for (const chunk of bodyChunks) {
+      const bodyEmbedding = await generatevectorEmbeddings({ text: chunk });
+      await insertIntoDb({ embedding: bodyEmbedding, url, head, body: chunk });
+    }
+  
+    for (const link of internalLinks) {
+      const _url = `${url}${link}`;
+      ingest(_url);
+    }
+  
+    console.log(`Ingested ${url} successfully`);
+  } catch (error) {
+    throw new Error('Could not insert into database for ' + url)
+  }
+}
+
+async function insertIntoDb({ embedding, url, body = "", head = "" }) {
+  const collection = await chromaClient.getOrCreateCollection({
+    name: WEB_COLLECTION,
+  });
+
+  await collection.add({
+    ids: [url],
+    embeddings: [embedding],
+    metadatas: [{ url, body, head }],
+  });
+}
+
+scrapewebappinfo(process.env.Scrape_URL);
+
 function chunkText(text, size) {
   if (!text || typeof text !== "string") {
     return [];
@@ -70,13 +121,3 @@ function chunkText(text, size) {
 
   return chunks;
 }
-
-const ingest = async (url = "") => {
-  const { head, body, internalLinks, externalLinks } = await scrapewebappinfo(
-    url
-  );
-  const HeadEmbeddings = await generatevectorEmbeddings(head);
-  const BodyEmbeddings = await generatevectorEmbeddings(body);
-};
-
-scrapewebappinfo(process.env.Scrape_URL);
